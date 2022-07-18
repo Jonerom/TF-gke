3102948c4783df2584867a100a5f1f9d052cd731
Jonerom <xowd69@gmail.com>
2022-07-18 20:17:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,28 @@
-module "gke" {
-  source                     = "terraform-google-modules/kubernetes-engine/google"
-  project_id                 = var.project_id
-  name                       = var.name
-  region                     = var.region
-  zones                      = var.zones
-  network                    = "default"
-  subnetwork                 = "default"
-  ip_range_pods              = ""
-  ip_range_services          = ""
-  http_load_balancing        = false
-  horizontal_pod_autoscaling = true
-  network_policy             = true
+resource "google_container_cluster" "primary" {
+  name                     = var.name
+  location                 = var.region
+  remove_default_node_pool = true
+  initial_node_count       = var.initial_node_count
+  min_master_version       = "1.20"
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${var.name}-node-pool"
+  cluster    = var.name
+  location   = var.region
+  node_count = var.initial_node_count
+
+  node_config {
+    preemptible  = false
+    machine_type = var.machine_type
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
 }
